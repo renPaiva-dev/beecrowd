213a09cf35c7055ac2779f92bedbcc0ba754f2e9
Renato <105134815+renPaiva-dev@users.noreply.github.com>
2025-06-22 15:01:35 -0300

    Update 1036.py
    adicionando resposta

--- python/1036.py
+++ python/1036.py
@@ -1,17 +1,21 @@
 def main():
-  entrada = float(input())
-  if entrada < 0:
-    print("Fora de intervalo")
-  elif entrada <= 25:
-    print("Intervalo [0,25]")
-  elif entrada <= 50:
-    print("Intervalo (25,50]")
-  elif entrada <= 75:
-    print("Intervalo [50,75]")
-  elif entrada <= 100:
-    print("Intervalo (75,100]")
-  else:
-    print("Fora de intervalo")
+  valores = input().split()
 
+  a = float(valores[0])
+  b = float(valores[1])
+  c = float(valores[2])
+
+  baskara(a,b,c)
+
+def baskara(a,b,c):
+  if a <= 0 or b <= 0 or c <= 0:
+    return print("Impossivel calcular")
+  delta = (b*b) - 4*a*c
+  if delta <= 0:
+    return print("Impossivel calcular")
+  r1 = (-b + delta**0.5) / (2*a)
+  r2 = (-b - delta**0.5) / (2*a)
+  print(f"R1 = {r1:.5f}")
+  print(f"R2 = {r2:.5f}")
 
 main()
